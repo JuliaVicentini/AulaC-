be1037082a8bf52b2642911cea57ae3828e67f41
JuliaVicentini <doamaralju@gmail.com>
2022-07-14 18:43:26 -0300

    Adicionei o depositar

--- meuPrimeiroProjeto/ContaCorrente.cs
+++ meuPrimeiroProjeto/ContaCorrente.cs
@@ -1,22 +1,46 @@
 public class ContaCorrente 
+// instanciar = criar objetos dentro de uma determinada classe, torná-la concreta
+// public = acessada de qualquer lugar do código 
 {
-    public string Titular;
+    public string Titular; //atributos da classe (características)
     public int Agencia;
     public int Numero;
     public double Saldo;
 
-    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo){ //construtor
-        this.Titular = Titular;
+    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo) 
+    //segue o nome da classe 
+    //construtor: possui as informações necessárias para a criação de um objeto dentro de uma classe. 
+    //Nesse caso, precisa haver, necessariamente, o titular, a agência,o  número e o saldo para criação da conta corrente. 
+    {
+        this.Titular = Titular; //o primeiro Titular é o atributo e o segundo é a variável 
         this.Agencia = Agencia;
-        this.Numero = Numero;
-        this.Saldo = Saldo; 
+        this.Numero = Numero; // this = representa o objeto que está sendo manipulado
+        this.Saldo = Saldo;
     }
 
-    public bool Sacar(double valorSaque){
-        if (this.Saldo >= valorSaque){
+    public bool Sacar(double valorSaque)
+    //método = sempre será verbo
+    // entre o tipo do método,'public', e o método, 'Sacar', há a informação do tipo de retorno.
+    // só há método quando houver um objeto atrelado a ele, por isso pode-se utilizar o 'this' para referenciar algum objeto 
+    {
+        if (this.Saldo >= valorSaque)
+        {
             this.Saldo = this.Saldo - valorSaque;
             return true;
         }
         return false;
     }
-}
+
+    public bool Depositar(double valorDeposito)
+    //método = sempre será verbo
+    // entre o tipo do método,'public', e o método, 'Sacar', há a informação do tipo de retorno.
+    // só há método quando houver um objeto atrelado a ele, por isso pode-se utilizar o 'this' para referenciar algum objeto 
+    {
+        if (valorDeposito > 0)
+        {
+            this.Saldo = this.Saldo + valorDeposito;
+            return true;
+        }
+        return false;
+    }
+} 
--- meuPrimeiroProjeto/Program.cs
+++ meuPrimeiroProjeto/Program.cs
@@ -6,37 +6,23 @@ namespace meuPrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDaJulia = new ContaCorrente  ("Júlia", 12345, 678, 100);
-            ContaCorrente contaDaFernanda = new ContaCorrente  ("Fernanda", 12345, 123, 10);
-            ContaCorrente contaDaRoberta = new ContaCorrente  ("Roberta", 12345, 790, 1000);
-            
-            Console.WriteLine("A conta é do (a) " + contaDaJulia.Titular + 
-            ", a agência é " + contaDaJulia.Agencia + " e o número é " 
-            + contaDaJulia.Numero + "Saldo: " + contaDaJulia.Saldo + ".");
-
-            Console.WriteLine("A conta é do (a) " + contaDaFernanda.Titular + 
-            ", a agência é " + contaDaFernanda.Agencia + " e o número é " 
-            + contaDaFernanda.Numero + "Saldo: " + contaDaFernanda.Saldo  + ".");
-
-            Console.WriteLine("A conta é do (a) " + contaDaRoberta.Titular + 
-            ", a agência é " + contaDaRoberta.Agencia + " e o número é " 
-            + contaDaRoberta.Numero +  "Saldo: " + contaDaRoberta.Saldo + ".");
-
-            bool sacar_conta1 = contaDaJulia.Sacar(100);
-            bool sacar_conta2 = contaDaFernanda.Sacar(100);
-            bool sacar_conta3 = contaDaRoberta.Sacar(100);
-
-            Console.WriteLine("A conta é do (a) " + contaDaJulia.Titular + 
-            ", a agência é " + contaDaJulia.Agencia + " e o número é " 
-            + contaDaJulia.Numero + "Saldo: " + contaDaJulia.Saldo + ".");
-
-            Console.WriteLine("A conta é do (a) " + contaDaFernanda.Titular + 
-            ", a agência é " + contaDaFernanda.Agencia + " e o número é " 
-            + contaDaFernanda.Numero + "Saldo: " + contaDaFernanda.Saldo  + ".");
-
-            Console.WriteLine("A conta é do (a) " + contaDaRoberta.Titular + 
-            ", a agência é " + contaDaRoberta.Agencia + " e o número é " 
-            + contaDaRoberta.Numero +  "Saldo: " + contaDaRoberta.Saldo + ".");
-        }
-    }
-}
+           ContaCorrente contaAna = new ContaCorrente ("Júlia", 123, 456, 1000); //passa a existir um novo objeto do tipo conta corrente 
+           Console.WriteLine("A conta é do(a): " +contaJulia.Titular+ ", a agência é: " +contaJulia.Agencia+ ", o número é: " +contaJulia.Numero+ " e o saldo é: " +contaJulia.Saldo+ "."); 
+           bool sacarContaJulia = contaJulia.Sacar(500); //o tipo da variável tem que ser do mesmo que o retorno atribuído ao método
+           Console.WriteLine("A conta é do(a): " +contaJulia.Titular+ ", a agência é: " +contaJulia.Agencia+ ", o número é: " +contaJulia.Numero+ " e o saldo é: " +contaJulia.Saldo+ "."); 
+           bool depositarContaJulia = contajulia.Depositar(300);
+           Console.WriteLine("A conta é do(a): " +contaJulia.Titular+ ", a agência é: " +contaJulia.Agencia+ ", o número é: " +contaJulia.Numero+ " e o saldo é: " +contaJulia.Saldo+ "."); 
+           
+           ContaCorrente contaFernanda = new ContaCorrente ("Fernanda", 123, 321, 190);  
+           Console.WriteLine("A conta é do(a): " +contaFernanda.Titular+ ", a agência é: " +contaFernanda.Agencia+ ", o número é: " +contaFernanda.Numero+ " e o saldo é: " +contaFernanda.Saldo+ "."); 
+           bool sacarcontaFernanda = contaFernanda.Sacar(50);
+           Console.WriteLine("A conta é do(a): " +contaFernanda.Titular+ ", a agência é: " +contaFernanda.Agencia+ ", o número é: " +contaFernanda.Numero+ " e o saldo é: " +contaFernanda.Saldo+ "."); 
+           bool depositarcontaFernanda = contaFernanda.Depositar(100);
+           Console.WriteLine("A conta é do(a): " +contaFernanda.Titular+ ", a agência é: " +contaFernanda.Agencia+ ", o número é: " +contaFernanda.Numero+ " e o saldo é: " +contaFernanda.Saldo+ "."); 
+           
+           ContaCorrente contaRoberta = new ContaCorrente ("Carlos", 123, 562, 500);  
+           Console.WriteLine("A conta é do(a): " +contaRoberta.Titular+ ", a agência é: " +contaRoberta.Agencia+ ", o número é: " +contaRoberta.Numero+ " e o saldo é: " +contaRoberta.Saldo+ "."); 
+           bool sacarcontaRoberta = contaRoberta.Sacar(500);
+           Console.WriteLine("A conta é do(a): " +contaRoberta.Titular+ ", a agência é: " +contaRoberta.Agencia+ ", o número é: " +contaRoberta.Numero+ " e o saldo é: " +contaRoberta.Saldo+ "."); 
+           bool depositarcontaRoberta = contaRoberta.Depositar(90);
+           Console.WriteLine("A conta é do(a): " +contaRoberta.Titular+ ", a agência é: " +contaRoberta.Agencia+ ", o número é: " +contaRoberta.Numero+ " e o saldo é: " +contaRoberta.Saldo+ ".");   
